87540a94463fd5f68cafb03202077e5dee41f895
cody s <cody@simtable.com>
2020-10-21 13:06:17 -0600

    commit #1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# parseLatLon
-Parse Latitude Longitude string
+[![view on npm](http://img.shields.io/npm/v/example.svg)](git@github.com:RedfishGroup/parseLatLon.git)
+
+ # Parse a string to a Longitude Latitude pair
+
+ ### 
+
+ Author: Cody Smith
+
+
+<a name="parse"></a>
+
+## parse(inputString) ⇒
+Parse a string to a Longitude Latitude pair
+
+### Supported Formats:
+* DD°MM.MMMM’
+* DD.DDDDDD°
+* DD° MM' SS"
+
+### Example:
+```
+parse(`35°25'01"N, 106°58'50"W`)
+parse("35.3434, -106.3434")
+```
+
+**Kind**: global function  
+**Returns**: [Longitude:Number,Latitude:Number] Longitude, Latitude  
+
+| Param | Type |
+| --- | --- |
+| inputString | <code>String</code> | 
+
+
+* * *
+
+&copy; 2020 Redifish Group LLC
---
+++ index.js
@@ -0,0 +1,80 @@
+//
+// Parse a string to a Longitude Latitude pair
+// © 2020 Redifish Group LLC
+
+/**
+ *  Parse a string to a Longitude Latitude pair
+ *
+ * ### Supported Formats:
+ * * DD°MM.MMMM’
+ * * DD.DDDDDD°
+ * * DD° MM' SS"
+ *
+ * ### Example:
+ * ```
+ * parse(`35°25'01"N, 106°58'50"W`)
+ * parse("35.3434, -106.3434")
+ * ```
+ * @export
+ * @param {String} inputString
+ * @return [Longitude:Number,Latitude:Number] Longitude, Latitude
+ */
+export function parse(inputString) {
+  const [lat, lon] = inputString.split(",");
+  if (
+    lon.toLowerCase().search("n") >= 0 ||
+    lon.toLowerCase().search("n") >= 0
+  ) {
+    //swap if in wrong order
+    const tmp = lat;
+    lat = lon;
+    lon = tmp;
+  }
+  return [parseCoor(lon), parseCoor(lat)];
+}
+
+function parseCoor(coor) {
+  const pf = parseFloat;
+  const coor2 = normalize(coor);
+  let result;
+  try {
+    const [a, b] = coor2.split("°");
+    const [c, d] = b.split(`'`);
+    // Deg: DD°MM.MMMM’
+    result = pf(a) + pf(b) / 60;
+    if (d) {
+      // DMS: DD° MM' SS"
+      result += pf(d) / 3600;
+    }
+  } catch (err) {
+    const coor3 = coor2.replace("°", "");
+    // Dec: DD.DDDDDD°
+    if (isNaN(pf(coor2))) {
+      throw err;
+    }
+    result = pf(coor2);
+  }
+  if (coor.toLowerCase().search("s") >= 0) result = result * -1;
+  if (coor.toLowerCase().search("w") >= 0) result = result * -1;
+
+  return result;
+}
+
+function normalize(coor) {
+  const result = coor
+    .replace(/\s+/gi, "")
+    .replace(/[swenNEWS]/gi, "")
+    .replace(/"/, "''")
+    .replace(/[°]$/gi, "");
+  return result;
+}
+
+//
+// test
+//
+// console.log([-120, 41], parse("41°N, 120°W"));
+// console.log([-120, 41], parse("-41, 120"));
+// console.log(
+//   [-120.98055555555555, 41.41694444444444],
+//   parse(`41°25'01"N, 120°58'50"W`)
+// );
